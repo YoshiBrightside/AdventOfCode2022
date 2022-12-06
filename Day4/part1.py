1043f3bba30c66433654df6420417ece87ca3961
Joshua Pedrero <pedrero.joshua@gmail.com>
2022-12-05 22:59:28 -0600

    Day 4
    Part 1, 2 and input for day 4.

---
+++ Day4/part1.py
@@ -0,0 +1,7 @@
+ans, line = 0, input().split(',')
+while(len(line) >= 2):
+    sect1, sect2 = [int(x) for x in line[0].split('-')] , [int(x) for x in line[1].split('-')]
+    if (sect1[0] <= sect2[0] and sect1[1] >= sect2[1]) or (sect2[0] <= sect1[0] and sect2[1] >= sect1[1]):
+        ans +=1
+    line = input().split(',')
+print(ans)
