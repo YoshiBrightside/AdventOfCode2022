d8f07b326f536a77da3ce99d92e98e5b6cd7b526
Joshua Pedrero <pedrero.joshua@gmail.com>
2022-12-07 21:40:10 -0600

    Day 6
    Part 1, 2 and input added

---
+++ Day6/part1.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def detect_start_of_packet(s):
+    ans = -1
+    cur_seq = {} # Storing the number of appearances inside our sequence
+    for i in range(len(s)):
+        if i >= 4:
+            if cur_seq[s[i - 4]] == 1:
+                cur_seq.pop(s[i - 4])
+            else:
+                cur_seq[s[i - 4]] -= 1
+        if s[i] in cur_seq:
+            cur_seq[s[i]] += 1
+        else:
+            cur_seq[s[i]] = 1
+        if len(cur_seq) == 4:
+            ans = i + 1
+            break
+    return ans
+        
+
+def main():
+    print(detect_start_of_packet(input()))
+
+if __name__ == "__main__":
+   main()
