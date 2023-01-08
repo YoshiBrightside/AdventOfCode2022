494a2ed73db1ee0fe37935e87c505283eb0d0f3a
Joshua Pedrero <pedrero.joshua@gmail.com>
2023-01-07 23:50:26 -0600

    Update part1.py

--- Day12/part1.py
+++ Day12/part1.py
@@ -16,30 +16,38 @@ class Heightmap:
         for i in range(len(self.heightmap)):
             for j in range(len(self.heightmap[0])):
                 if self.heightmap == value:
+                    # Save coords and replace S and E with respective elevation
+                    self.heightmap[i][j] = 'a' if value == 'S' else 'z'
                     return i, j
         return None
 
     def find_shortest_path(self):
-        shortest_path = [[-1 for  _ in range(len(self.heightmap[0]))] for _ in range(len(self.heightmap))]
-        seen = set([self._exit])
+        shortest_path = {self._exit: 0}
         queue = deque([self._exit])
         shortest_path[self._exit[0]][self._exit[1]] = 0
         steps = 0
+        next_queue = deque()
         while queue:
             coords = queue.popleft()
+            shortest_path[coords] = steps
             possible_moves = ([(coords[0] + 1, coords[1]), (coords[0], coords[1] + 1),
                                (coords[0] - 1, coords[1]), (coords[0], coords[1] - 1)])
             for x, y in possible_moves:
                 if self.are_valid_neightbours(coords[0], coords[1], x, y):
-                    
-
-        return ans
-        # uses deques to do a BFS starting from E
+                    if (x, y) not in shortest_path:
+                        shortest_path[(x, y)] = -1
+                        next_queue.append((x, y))
+            if not queue:
+                queue = next_queue
+                next_queue = deque()
+        return shortest_path
     
     def are_valid_neightbours(self, x1, y1, x2, y2):
-        return (x2 > 0 and x2 < self.heightmap and
-                y2 > 0 and y1 < self.heightmap[0] and
-                abs(self.heightmap[x1][y1] - self.heightmap[x2][y2]) <= 1) # We gonna need to convert ascii to int
+        if (x2 > 0 and x2 < self.heightmap and
+            y2 > 0 and y1 < self.heightmap[0] and
+            abs(ord(self.heightmap[x1][y1]) - ord(self.heightmap[x2][y2]) <= 1)):
+            return True
+        return False
 
     
 
