55b70267432a45fa58b606e2f0ab22e0b299582a
Joshua Pedrero <pedrero.joshua@gmail.com>
2023-01-25 23:00:04 -0600

    Update part1.py

--- Day13/part1.py
+++ Day13/part1.py
@@ -1,6 +1,6 @@
 '''
-Part 1 of Day 12, Hill Climbing Algorithm
-https://adventofcode.com/2022/day/11
+Part 1 of Day 13, Distress Signal
+https://adventofcode.com/2022/day/13
 '''
 
 import ast
@@ -14,20 +14,29 @@ def get_signal():
     return signal
 
 def evaluate_packets(pckt1, pckt2):
+    '''Compare packets and returns if they are in the right order.'''
     for i in range(max(len(pckt1), len(pckt2))):
-        if len(pckt1) == i: return True
-        if len(pckt2) == i: return False
-        if type(pckt1[i]) == int and type(pckt2[i]) == int:
-            if pckt1[i] < pckt2[i]: return True
-            if pckt1[i] > pckt2[i]: return False
-        if type(pckt1[i]) == list or type(pckt2[i]) == list:
-            if type(pckt1[i]) != list: pckt1[i] = [pckt1[i]]
-            if type(pckt2[i]) != list: pckt2[i] = [pckt2[i]]
+        if len(pckt1) == i:
+            return True
+        if len(pckt2) == i:
+            return False
+        if isinstance(pckt1[i], int) and isinstance(pckt2[i], int):
+            if pckt1[i] < pckt2[i]:
+                return True
+            if pckt1[i] > pckt2[i]:
+                return False
+        if isinstance(pckt1[i], list) or isinstance(pckt2[i], list):
+            if not isinstance(pckt1[i], list):
+                pckt1[i] = [pckt1[i]]
+            if not isinstance(pckt2[i], list):
+                pckt2[i] = [pckt2[i]]
             ans = evaluate_packets(pckt1[i], pckt2[i])
-            if ans is not None: return ans
+            if ans is not None:
+                return ans
     return None
 
 def correct_pairs_indices(signal):
+    '''Returns the list of pairs in the right order.'''
     ans = []
     for i in range(0, len(signal), 2):
         pckt1, pckt2 = signal[i], signal[i+1]
@@ -37,6 +46,7 @@ def correct_pairs_indices(signal):
 
 
 def main():
+    '''Retturns the sum of the indices of the pairs in the right order.'''
     signal = get_signal()
     ans = correct_pairs_indices(signal)
     print(sum(ans))
