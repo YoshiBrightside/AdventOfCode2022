cfaa46b83165d47873a079bc9465e823eaa6bc21
Joshua Pedrero <pedrero.joshua@gmail.com>
2023-02-03 11:51:44 -0600

    Create part1.py

---
+++ Day14/part1.py
@@ -0,0 +1,20 @@
+'''
+Part 1 of Day 14, Regolith Reservoir.
+https://adventofcode.com/2022/day/13
+'''
+
+
+def get_signal():
+    '''Reads signal packages and pass it as lists of list.'''
+    signal = []
+    for line in open("Day14\input", mode='r').read().split('\n'):
+        continue
+    return signal
+
+def main():
+    '''Returns the sum of the indices of the pairs in the right order.'''
+    signal = get_signal()
+    return
+
+if __name__ == "__main__":
+    main()
