db746a723e372e7aee2302f269781296a4c421e4
Joshua Pedrero <pedrero.joshua@gmail.com>
2023-02-09 23:41:02 -0600

    Update part1.py

--- Day14/part1.py
+++ Day14/part1.py
@@ -50,10 +50,12 @@ def create_matrix(structures, min_max_coords):
             print(i, structure[i], structure[i+1])
             if structure[i][0] == structure[i+1][0]:
                 for j in range(min(structure[i][1], structure[i+1][1]), max(structure[i][1], structure[i+1][1])+1):
-                    matrix[structure[i][0] - left_x][structure[i][1] + j] = 1
+                    print(j, structure[i][0], structure[i][1], structure[i+1][0], structure[i+1][1])
+                    matrix[structure[i][0] - left_x][max(structure[i][1], structure[i+1][1]) - min(structure[i][1], structure[i+1][1]) + j] = 1
             else:
-                for j in range(min(structure[i][0], structure[i+1][0]), max(structure[i][0], structure[i+1][0])+1):
-                     matrix[structure[i][0] - left_x + j][structure[i][1]] = 1
+                for j in range(min(structure[i][0], structure[i+1][0]), max(structure[i][0], structure[i+1][0])+1): # 498,4 -> 498,6 -> 496,6
+                    print(j, structure[i][0], structure[i][1], structure[i+1][0], structure[i+1][1])
+                    matrix[max(structure[i][0], structure[i+1][0]) - left_x + j][structure[i][1]] = 1
     return matrix
 
 def get_min_max_coords(structures):
